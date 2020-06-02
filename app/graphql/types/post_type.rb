8a0a9e7b191dd601c19942131181b98a9f1403ff
sudeepsnairails <sudeepsnair.career@gmail.com>
2020-06-02 22:53:42 +0530

    added graphql examples

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,4 +1,9 @@
 module Types
   class PostType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: true
+    field :body, String, null: true
+
+    field :user, UserType, null: true
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,9 @@
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :email, String, null: true
+
+    field :posts, [Types::PostType], null: true
   end
 end
